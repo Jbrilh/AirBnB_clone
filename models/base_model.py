9e9a8e47ece3adecee71191287a92d75fc32d14a
Jbril <Jbrilhdeveloper@gmail.com>
2024-01-12 06:29:21 +0000

    base model updated

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,20 @@ class BaseModel:
     """Basemodel class defines all common attributes
        and methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """basemodel class inintalized"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """return the string rep of the class"""
@@ -27,10 +36,15 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary key/value pair of __dict__"""
-        new_dict = dict(self.__dict__)
-        new_dict['id'] = self.id
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        new_dict['__class__'] = self.__class__.__name__
+        obj_dict = {
+                'id': self.id,
+                'created_at': self.created_at.isoformat(),
+                'updated_at': self.updated_at.isoformat(),
+                '__class__': self.__class__.__name__
+                }
 
-        return new_dict
+        for key, value in self.__dict__.items():
+            if key not in obj_dict:
+                obj_dict[key] = value
+
+        return obj_dict
